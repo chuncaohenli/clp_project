f2b311326738dbd4700fa733015a7115698ead1a
chuncaohenli <604265882@qq.com>
2017-03-08 16:18:19 -0800

    set product id as a var

--- readjson_demo.py
+++ readjson_demo.py
@@ -5,12 +5,13 @@ from Item import *
 fopen = open('jd_review/3133817_0.json','r')
 content = fopen.readline()
 jcontent = json.loads(content)
+productID = '3133817'
 
 # read 10 comments
 def getComments(jcontent):
     res = []
     for cl in jcontent['comments']:
-        tmp = Comment(cl['content'],cl['creationTime'],cl['id'],'3133817')
+        tmp = Comment(cl['content'],cl['creationTime'],cl['id'],productID)
         res.append(tmp)
     return res
 # read hot tags for this product
